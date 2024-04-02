83b7d1651904ddb238bd9281b91ce7983aac784f
Krishnakali-Dutta <159030689+Krishnakali-Dutta@users.noreply.github.com>
2024-04-02 11:54:18 +0530

    Update StartApplication.java

--- src/main/java/com/krishnakali/StartApplication.java
+++ src/main/java/com/krishnakali/StartApplication.java
@@ -12,15 +12,13 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfully built a Spring Boot application using Maven!!!! ");
-        model.addAttribute("msg", "This application is deployed onto EKS cluster using Helm and Jenkins Pipeline!!!!");
+        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
+        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
         return "index";
     }
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(StartApplication.class);
-        app.setDefaultProperties(Collections.singletonMap("server.servlet.context-path", "/myapp"));
-        app.run(args);
+        SpringApplication.run(StartApplication.class, args);
     }
 
 }
